17ce64a3fec824146df7ae6560a7682ef4b25d93
Casey DeLorme <cdelorme@gmail.com>
2014-10-20 03:49:04 -0400

    continuing to expand outline of primary code

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ import (
 
 // temporary settings wrapper
 type Dedup struct {
-	Delete bool
+	Path   string
 	Quiet  bool
+	Delete bool
 	Move   string
 }
 
@@ -27,15 +28,38 @@ func main() {
 
 	// prepare cli options
 	appOptions := option.App{Description: "file deduplication program"}
+	appOptions.Flag("path", "path to begin scanning", "-p", "--path")
 	appOptions.Flag("quiet", "silence output", "-q", "--quiet")
 	appOptions.Flag("delete", "delete duplicate files", "-d", "--delete")
 	appOptions.Flag("move", "move files to supplied path", "-m", "--move")
+	// add a concurrency flag?
 	o := appOptions.Parse()
 
+	d.Path, _ = maps.String(&o, "", "path")
 	d.Quiet, _ = maps.Bool(&o, false, "quiet")
 	d.Delete, _ = maps.Bool(&o, false, "delete")
 	d.Move, _ = maps.String(&o, "", "move")
 
 	logger.Info("Dedup State: %+v", d)
 
+	// steps to implement
+	// - recurse directory
+	// - build list of file paths
+	// - store sizes with file paths
+	// - sort files to group by size
+	// - concurrently run through each group
+	// - hash file contents and compare for each group
+
+}
+
+func (dedup *Dedup) Walk() {
+	// walk a supplied path and build a single map of file paths plus sizes
+}
+
+func (dedup *Dedup) Sort() {
+	// sort files by size & return a map of grouped maps
+}
+
+func (dedup *Dedup) Process() {
+	// spinup a queue of go routines to process each group individually
 }
