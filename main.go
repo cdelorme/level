a78557c8b73f68e5342414423f15b8524a6d2e7a
Casey DeLorme <cdelorme@gmail.com>
2014-10-20 03:33:09 -0400

    created initial outline for the application

--- main.go
+++ main.go
@@ -1,63 +1,41 @@
 package main
 
 import (
-    "os"
-    "fmt"
-    "strings"
-    "path"
-    "os/user"
+	// "fmt"
+	// "os"
+	// "strings"
 
-    "github.com/cdelorme/go-log"
-    "github.com/cdelorme/go-config"
+	"github.com/cdelorme/go-log"
+	"github.com/cdelorme/go-maps"
+	"github.com/cdelorme/go-option"
 )
 
+// temporary settings wrapper
 type Dedup struct {
-    Delete bool
-    Quiet bool
-    Move string
+	Delete bool
+	Quiet  bool
+	Move   string
 }
-
 
 func main() {
 
-    // prepare new dedup struct
-    d := Dedup{}
+	// prepare new dedup struct
+	d := Dedup{}
 
-    // prepare logger
-    logger := log.Logger{};
+	// prepare logger
+	logger := log.Logger{}
 
-    // get user to acquire homedir
-    u, e := user.Current()
-    if e != nil {
-        logger.Critical("No user?  Bugging out")
-        os.Exit(1)
-    }
+	// prepare cli options
+	appOptions := option.App{Description: "file deduplication program"}
+	appOptions.Flag("quiet", "silence output", "-q", "--quiet")
+	appOptions.Flag("delete", "delete duplicate files", "-d", "--delete")
+	appOptions.Flag("move", "move files to supplied path", "-m", "--move")
+	o := appOptions.Parse()
 
-    // attempt to load config data (my config library should totally assume default config file names by application)
-    conf := config.Config{File: u.HomeDir + "/." + path.Base(os.Args[0])}
-    conf.Load()
+	d.Quiet, _ = maps.Bool(&o, false, "quiet")
+	d.Delete, _ = maps.Bool(&o, false, "delete")
+	d.Move, _ = maps.String(&o, "", "move")
 
-    // apply config or defaults
-    // @todo (config lib missing bool option)
+	logger.Info("Dedup State: %+v", d)
 
-    // manually parsed flags
-    for _, arg := range os.Args {
-        if (arg == "-h" || arg == "--help") {
-            // still trying to figure out how to override the default help info...
-            fmt.Printf("%20s, %s\n", "-h | --help", "Print this usage information")
-            fmt.Printf("%20s, %s\n", "-q | --quiet", "Quiet mode, no log output, will assume delete")
-            fmt.Printf("%20s, %s\n", "-d | --delete", "Delete duplicate files")
-            fmt.Printf("%20s, %s\n", "-m | --move", "Move duplicate files to supplied path")
-            fmt.Printf("Example: %s\n", "")
-        } else if arg == "-d" || arg == "--delete" {
-            d.Delete = true
-        } else if arg == "-q" || arg == "--quiet" {
-            d.Quiet = true
-        } else if arg == "-m" || strings.HasPrefix(arg, "--move") {
-            fmt.Println("Moving Files")
-            // need to add parsing logic here
-        }
-    }
-
-    logger.Info("Test: %+v", d);
 }
