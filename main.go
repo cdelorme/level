e54a46316ccdf95349cd94491b521310493636a8
Casey DeLorme <cdelorme@gmail.com>
2014-10-30 18:30:13 -0400

    updated deduplication process, hashing works but does not yet assign

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -25,12 +28,17 @@ type Dedup struct {
 	Delete         bool
 	Move           string
 	Files          map[int64][]File
+	Duplicates     map[string][]File
 }
 
 func main() {
 
-	// prepare new dedup struct /w logger & empty file map
-	d := Dedup{Logger: log.Logger{Level: log.INFO}, Files: make(map[int64][]File)}
+	// prepare new dedup struct /w logger, and empty files/dedup maps
+	d := Dedup{
+		Logger:     log.Logger{Level: log.INFO},
+		Files:      make(map[int64][]File),
+		Duplicates: make(map[string][]File),
+	}
 
 	// optimize concurrent processing
 	d.MaxParallelism = runtime.NumCPU()
@@ -58,25 +66,31 @@ func main() {
 	}
 
 	// print state of dedup before we continue
-	d.Logger.Debug("Dedup State: %+v", d)
+	d.Logger.Debug("initial application state: %+v", d)
 
 	// test directory walk
 	if err := filepath.Walk(d.Path, d.Walk); err != nil {
-		d.Logger.Error("Failed to walk directory: %s", err)
+		d.Logger.Error("failed to walk directory: %s", err)
 	}
 
 	// print list of files grouped by size index
-	d.Logger.Debug("Files: %+v", d.Files)
+	d.Logger.Debug("files: %+v", d.Files)
 
 	// run comparison process
 	d.ComparisonProcess()
 
-	d.Logger.Critical("Testing %d and %d", 1, 4)
+	// print duplicates
+	d.Logger.Debug("duplicates: %+v", d.Duplicates)
+	d.Logger.Debug("files: %+v", d.Files)
 
-	// steps to implement
-	// - concurrently run through each group
-	// - hash file contents and compare for each group
-
+	// @todo
+	// - fix broken hash assignment (not being attached to files)
+	// - for optimal safety, run sort-by-hash logic after hash generation & move hash comparison to a second set of concurrent tasks
+	// - fix and optimize broken deduplication comparison loops
+	// - add methods to handle delete || move logic
+	//     - if delete is set, move is ignored
+	//     - move will generate folders with hash names and move files over, and must handle name conflicts (or simply number the files)
+	//     - delete will remove all but the (first || last) indexed item
 }
 
 func (dedup *Dedup) Walk(path string, file os.FileInfo, err error) error {
@@ -115,8 +129,42 @@ func (dedup *Dedup) ComparisonProcess() {
 func (dedup *Dedup) HashAndCompare(sizes chan int64, wg *sync.WaitGroup, num int) {
 	defer wg.Done()
 
+	// if we can share a single Hash class (one per channel) then we could improve this process
+
 	// iterate each supplied size
 	for size := range sizes {
-		dedup.Logger.Debug("Channel %d, size %d", num, size)
+		dedup.Logger.Debug("channel %d, size %d", num, size)
+
+		for _, file := range dedup.Files[size] {
+			content, err := ioutil.ReadFile(file.Path)
+			if err != nil {
+				dedup.Logger.Error("failed to parse file %s, %s", file.Path, err)
+				continue
+			}
+
+			// create the hash
+			hash := sha256.New()
+			hash.Write(content)
+			file.Hash = hex.EncodeToString(hash.Sum(nil))
+			dedup.Logger.Debug("file %s hash %s", file.Path, file.Hash)
+		}
+
+		// run hash comparison to build a list of duplicates
+		// for _, f := range dedup.Files[size] {
+		// 	b := false
+		// 	for _, d := range dedup.Files[size] {
+		// 		if f != d && f.Hash == d.Hash {
+		// 			if _, ok := dedup.Duplicates[f.Hash]; !ok {
+		// 				dedup.Duplicates[f.Hash] = make([]File, 0)
+		// 			}
+		// 			dedup.Duplicates[f.Hash] = append(dedup.Duplicates[f.Hash], d)
+		// 			b = true
+		// 		}
+		// 		if b {
+		// 			dedup.Duplicates[f.Hash] = append(dedup.Duplicates[f.Hash], f)
+		// 		}
+		// 	}
+		// }
+
 	}
 }
