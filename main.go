26613120d3e6ab7e03abdfe80a03b13c6941cddc
Casey DeLorme <cdelorme@gmail.com>
2014-10-31 22:18:04 -0400

    embedded goroutines as anon functions

--- main.go
+++ main.go
@@ -92,11 +92,11 @@ func main() {
 	}
 	level6.Logger.Debug("files: %+v", level6.Files)
 
-	level6.GenerateHashesAsync()
+	level6.GenerateHashes()
 	level6.Logger.Debug("files /w hashes: %+v", level6.Files)
 
 	// async compare
-	level6.CompareHashesAsync()
+	level6.CompareHashes()
 	level6.Logger.Debug("duplicates: %+v", level6.Duplicates)
 
 	// print out contents as pretty json (readable, and programmatically parsable)
@@ -117,8 +117,8 @@ func main() {
 	}
 
 	// @todo
-	// - move goroutine functions inline to async methods, and rename async to general-name
-	// - test moving & deletion on large data sets (30~GB)
+	// - separate structs and related methods to their own files
+	// - write move & delete logic, and test on decent sized data sets (30~GB)
 }
 
 func (level6 *Level6) Walk(path string, file os.FileInfo, err error) error {
@@ -132,72 +132,92 @@ func (level6 *Level6) Walk(path string, file os.FileInfo, err error) error {
 	return err
 }
 
-func (level6 *Level6) GenerateHashesAsync() {
+func (level6 *Level6) GenerateHashes() {
 
 	// prepare tasks channel and wait group for concurrent processing
-	tasks := make(chan int64)
+	sizes := make(chan int64)
 	var wg sync.WaitGroup
 
 	// prepare go routines and add to wait group
 	for i := 0; i < level6.MaxParallelism; i++ {
 		wg.Add(1)
-		go level6.GenerateHashes(tasks, &wg, i)
+		go func() {
+			defer wg.Done()
+
+			// @todo test single hash instance
+			hash := sha256.New()
+
+			// iterate each supplied size
+			for size := range sizes {
+				level6.Logger.Debug("channel %d, for file size %d", i, size)
+				for i, _ := range level6.Files[size] {
+					content, err := ioutil.ReadFile(level6.Files[size][i].Path)
+					if err != nil {
+						level6.Logger.Error("failed to parse file %s, %s", level6.Files[size][i].Path, err)
+						continue
+					}
+
+					// generate hash, add to instance, and reset hash
+					hash.Write(content)
+					level6.Files[size][i].Hash = hex.EncodeToString(hash.Sum(nil))
+					hash.Reset()
+				}
+			}
+		}()
 	}
 
 	// send sets of files to each go routine
 	for size, _ := range level6.Files {
 		if len(level6.Files[size]) > 1 {
-			tasks <- size
+			sizes <- size
 		}
 	}
 
 	// close channels and wait for done() calls before moving forward
-	close(tasks)
+	close(sizes)
 	wg.Wait()
 }
 
-func (level6 *Level6) GenerateHashes(sizes chan int64, wg *sync.WaitGroup, num int) {
-	defer wg.Done()
-
-	// @todo test single hash instance
-	hash := sha256.New()
-
-	// iterate each supplied size
-	for size := range sizes {
-		level6.Logger.Debug("channel %d, for file size %d", num, size)
-		for i, _ := range level6.Files[size] {
-			content, err := ioutil.ReadFile(level6.Files[size][i].Path)
-			if err != nil {
-				level6.Logger.Error("failed to parse file %s, %s", level6.Files[size][i].Path, err)
-				continue
-			}
-
-			// generate hash, add to instance, and reset hash
-			hash.Write(content)
-			level6.Files[size][i].Hash = hex.EncodeToString(hash.Sum(nil))
-			hash.Reset()
-		}
-	}
-}
-
-func (level6 *Level6) CompareHashesAsync() {
+func (level6 *Level6) CompareHashes() {
 
 	// prepare tasks and duplicates with a wait group
-	tasks := make(chan int64)
+	sizes := make(chan int64)
 	duplicates := make(chan map[string][]File)
 	var wg sync.WaitGroup
 
 	// prepare go routines and add to wait group
 	for i := 0; i < level6.MaxParallelism; i++ {
 		wg.Add(1)
-		go level6.CompareHashes(tasks, duplicates, &wg, i)
+		// go level6.CompareHashes(tasks, duplicates, &wg, i)
+		go func() {
+			defer wg.Done()
+			for size := range sizes {
+				dups := make(map[string][]File)
+				for i, _ := range level6.Files[size] {
+					if _, ok := dups[level6.Files[size][i].Hash]; !ok {
+						dups[level6.Files[size][i].Hash] = make([]File, 0)
+						for d := i + 1; d < len(level6.Files[size]); d++ {
+							if level6.Files[size][i].Hash == level6.Files[size][d].Hash {
+								dups[level6.Files[size][i].Hash] = append(dups[level6.Files[size][i].Hash], level6.Files[size][d])
+							}
+						}
+						if len(dups[level6.Files[size][i].Hash]) > 0 {
+							dups[level6.Files[size][i].Hash] = append(dups[level6.Files[size][i].Hash], level6.Files[size][i])
+						}
+					}
+				}
+				if len(dups) > 0 {
+					duplicates <- dups
+				}
+			}
+		}()
 	}
 
 	// asynchronously capture duplicates but synchronously & safely append to level6.Duplicates
 	go func() {
 		for {
-			data := <-duplicates
-			for hash, files := range data {
+			dups := <-duplicates
+			for hash, files := range dups {
 				if len(files) > 0 {
 					if _, ok := level6.Duplicates[hash]; !ok {
 						level6.Duplicates[hash] = make([]File, 0)
@@ -211,35 +231,12 @@ func (level6 *Level6) CompareHashesAsync() {
 	// send sizes to our channel and let our goroutines pick up the work
 	for size, _ := range level6.Files {
 		if len(level6.Files[size]) > 1 {
-			tasks <- size
+			sizes <- size
 		}
 	}
 
 	// when all of our tasks are done, we can wait for completion then close our output
-	close(tasks)
+	close(sizes)
 	wg.Wait()
 	close(duplicates)
-}
-
-func (level6 *Level6) CompareHashes(sizes chan int64, duplicates chan map[string][]File, wg *sync.WaitGroup, num int) {
-	defer wg.Done()
-	for size := range sizes {
-		dups := make(map[string][]File)
-		for i, _ := range level6.Files[size] {
-			if _, ok := dups[level6.Files[size][i].Hash]; !ok {
-				dups[level6.Files[size][i].Hash] = make([]File, 0)
-				for d := i + 1; d < len(level6.Files[size]); d++ {
-					if level6.Files[size][i].Hash == level6.Files[size][d].Hash {
-						dups[level6.Files[size][i].Hash] = append(dups[level6.Files[size][i].Hash], level6.Files[size][d])
-					}
-				}
-				if len(dups[level6.Files[size][i].Hash]) > 0 {
-					dups[level6.Files[size][i].Hash] = append(dups[level6.Files[size][i].Hash], level6.Files[size][i])
-				}
-			}
-		}
-		if len(dups) > 0 {
-			duplicates <- dups
-		}
-	}
 }
