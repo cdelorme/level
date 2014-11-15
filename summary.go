3cf5d3d4de233b1ca703135287edddccc0f7858e
Casey DeLorme <cdelorme@gmail.com>
2014-11-15 11:35:43 -0800

    aesthetic correction

--- summary.go
+++ summary.go
@@ -4,11 +4,11 @@ import "time"
 
 type Summary struct {
 	Files            int64
-    Crc32Hashes      int64
+	Crc32Hashes      int64
 	Crc32Duplicates  int64
-    Sha256Hashes     int64
+	Sha256Hashes     int64
 	Sha256Duplicates int64
 	Moves            int64
 	Deletes          int64
-	Time            time.Time
+	Time             time.Time
 }
