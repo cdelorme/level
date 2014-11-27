6555840570f0d8d51022c30655999ac45dc3c870
Casey DeLorme <cdelorme@gmail.com>
2014-11-27 02:36:33 -0500

    updated summary container components

--- summary.go
+++ summary.go
@@ -3,13 +3,12 @@ package main
 import "time"
 
 type Summary struct {
-	Files            int64
-	Crc32Hashes      int64
-	Crc32Duplicates  int64
-	Sha256Hashes     int64
-	Sha256Duplicates int64
-	Moves            int64
-	Deletes          int64
-	Start            time.Time
-	Time             time.Duration
+	Files        int64
+	Crc32Hashes  int64
+	Sha256Hashes int64
+	Duplicates   int64
+	Moves        int64
+	Deletes      int64
+	Start        time.Time
+	Time         time.Duration
 }
