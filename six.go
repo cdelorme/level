03564fbe37bdfb368e6a1d65c0f75352c58a1a2c
cdelorme <cdelorme@gmail.com>
2017-06-12 09:35:38 -0400

    collect total found duplicates (after byte-by-byte)

--- six.go
+++ six.go
@@ -167,6 +167,7 @@ func (s *Six) Data(m map[int64][]string) [][]string {
 					}
 				}
 				if len(match) > 0 {
+					s.Stats.Add("Total Duplicate Files", len(match))
 					duplicates = append(duplicates, append([]string{set[j]}, match...))
 				}
 			}
